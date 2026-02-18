b02254330aa368a59146dd7ec5b4582d4720bd15
Giancarlo Sena Martins Junior <giancarlo456@hotmail.com>
2026-02-18 15:38:55 -0300

    fix: correção do VoteResquest para aceitar apenas numeros e 11 digitos

--- src/main/java/com/test/voting/dto/VoteRequest.java
+++ src/main/java/com/test/voting/dto/VoteRequest.java
@@ -3,12 +3,14 @@ package com.test.voting.dto;
 import com.test.voting.model.enums.VoteType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record VoteRequest(
     @NotNull(message = "Session ID is required")
     Long sessionId,
     
     @NotBlank(message = "CPF is required")
+    @Pattern(regexp = "^\\d{11}$", message = "CPF must contain exactly 11 numbers")
     String cpf,
     
     @NotNull(message = "Vote is required")
